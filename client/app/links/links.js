71a25a9905b2117ad50f22f64d080a4e559ec6f7
cynthiaechen <cynthiaechen@gmail.com>
2015-09-18 16:35:20 -0700

    Passed all tests!

--- client/app/links/links.js
+++ client/app/links/links.js
@@ -1,5 +1,13 @@
 angular.module('shortly.links', [])
 
 .controller('LinksController', function ($scope, Links) {
-  // Your code here
+  $scope.data = {};
+
+  $scope.getLinks = function () {
+    Links.linkGetter().then(function (dataLinks) {
+      $scope.data.links = dataLinks;
+    });
+  };
+
+  $scope.getLinks();
 });
--- client/app/services/services.js
+++ client/app/services/services.js
@@ -1,8 +1,35 @@
 angular.module('shortly.services', [])
 
 .factory('Links', function ($http) {
-  // Your code here
+    var linkGetter = function () {
+    return $http({
+      method: 'GET',
+      url: '/api/links'
+    })
+    .then(function (resp) {
+      return resp.data;
+    });
+  };
+
+
+  var linkPoster = function (link) {
+    return $http({
+      method: 'POST',
+      url: '/api/links',
+      data: link
+    })
+    .then(function (resp) {
+      console.log(resp);
+      return resp.status;
+    });
+  };
+
+  return {
+    linkGetter: linkGetter,
+    linkPoster: linkPoster
+  };
 })
+
 .factory('Auth', function ($http, $location, $window) {
   // Don't touch this Auth service!!!
   // it is responsible for authenticating our user
