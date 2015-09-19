8bea1526426cae3fbf1a7e0a2118a4c910ff7209
cynthiaechen <cynthiaechen@gmail.com>
2015-09-18 20:17:58 -0700

    "Shortened links and rendered them"

--- client/app/shorten/shorten.js
+++ client/app/shorten/shorten.js
@@ -1,10 +1,16 @@
-angular.module('shortly.shorten', [])
+var utils = require('../../../server/config/utils.js');
+
+angular.module('shortly.shorten', ['shortly.shorten.utils'])
 
 .controller('ShortenController', function ($scope, $location, Links) {
   $scope.link = {};
-
-  $scope.addLink = function () {
-    Links.linkPoster();
+  // $scope.link.links = [];
+  $scope.addLink = function (link) {
+    $scope.link.url = link;
+    // $scope.link.links.push(link);
+    Links.linkPoster($scope.link);
   };
+
+  $scope.isUrl = utils.isValidUrl;
 
 });
